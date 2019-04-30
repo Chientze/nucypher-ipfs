6018c6d9032a168e59b0e0a35f5c2e7066e6be8f
Pisuth Daengthongdee <pisuth.dae@gmail.com>
2019-04-30 14:05:42 +0700

    added install_require

--- setup.py
+++ setup.py
@@ -13,6 +13,10 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/pisuthd/nucypher-ipfs",
     packages=setuptools.find_packages(),
+    install_requires=[
+          'ipfs-api',
+          'nucypher'
+      ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
